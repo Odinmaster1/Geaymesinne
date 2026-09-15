9bc8f9a3c86df3b1e0d9e4a9972496b6fec07fd1
Williams <1728861@mukilteo.wednet.edu>
2026-09-15 08:29:04 -0700

    sdg

---
+++ Assets/PlatFormerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlatFormerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+
+    public Rigidbody2D rb2d;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+
+    }
+}
