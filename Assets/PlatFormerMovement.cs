42c5a6278ef41a663efba0bc5a3a03e829975c48
Odinmaster1 <min3craft.jakob@gmail.com>
2026-09-17 07:55:06 -0700

    Worked on the movement somewhat

--- Assets/PlatFormerMovement.cs
+++ Assets/PlatFormerMovement.cs
@@ -3,30 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlatFormerMovement : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
     public float jumpHeight;
 
-    public Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
+
+    private float _movement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb2d.linearVelocityX = _movement;
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
-
+        _movement = ctx.ReadValue<Vector2>().x * moveSpeed;
     }
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-
+        if (ctx.ReadValue<float>() == 1)
+        {
+        rb2d.linearVelocityY = jumpHeight;
+        }
     }
 }
