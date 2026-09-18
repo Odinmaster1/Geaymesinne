3ae69570076182ccf635a14e291d13502dee950a
Odinmaster1 <min3craft.jakob@gmail.com>
2026-09-18 09:13:41 -0700

    Worked on trying to limit the jumps

--- Assets/jakob/PlatFormerMovement.cs
+++ Assets/jakob/PlatFormerMovement.cs
@@ -3,8 +3,11 @@ using UnityEngine.InputSystem;
 
 public class PlatFormerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpHeight;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private Vector2 _boxOffset;
+    [SerializeField] private Vector2 _boxSize;
+    [SerializeField] private LayerMask _boxLayer;
 
     private Rigidbody2D rb2d;
 
@@ -29,9 +32,23 @@ public class PlatFormerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-        if (ctx.ReadValue<float>() == 1)
+        if (ctx.ReadValue<float>() == 1  || !IsGrounded())
         {
         rb2d.linearVelocityY = jumpHeight;
         }
+    }
+
+    private bool IsGrounded()
+    {
+        // Check for ground using a Boxcast.
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + (Vector3) _boxOffset, _boxSize, 0, Vector2.zero,  0,  _boxLayer);
+        Debug.Log(hit.collider.name);
+
+        return hit;
+    }
+
+    private void OnDrawGizmos()
+    {
+        
     }
 }
